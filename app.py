42dc0d4e65916252817a27654986c5f471d37f7a
东风 <zhangyang_2002@foxmail.com>
2024-07-07 00:52:13 +0800

    Refine the code

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from utils import batch_upload_to_wordpress
+from utils import batch_upload_to_wordpress, search_all_file
 
 ## 文件夹路径,b开头的字符串是转义字符,记得在前面加\转义,例如\\blog
-folder_path = "G:\code\\blog\YuQueBackups\\backups\origin_md\博客"
+folder_path = "G:\code\\blog\\note-book\个人理解"
 ## 无用的文件夹前缀
-useless_folder_prefix = "G:\\code\\blog\\YuQueBackups\\backups\\origin_md\\博客\\"
+useless_folder_prefix = "G:\code\\blog\\note-book\个人理解\\"
 ## 文件夹分隔符
 split_symbol = "\\"
 ## 需要获取的文件后缀
@@ -15,4 +15,18 @@ if __name__ == "__main__":
     site_url = "https://yourwordpresssite.com/xmlrpc.php"
     username = "your_username"
     password = "your_password"
-    batch_upload_to_wordpress(folder_path, site_url, username, password)
+    fileList = search_all_file(folder_path, suffix_tuple)
+    print("共扫描到{}个文件".format(len(fileList)))
+    isUpload = input("请输入是否上传所有文件(y or n)：")
+    if isUpload == "y":
+        batch_upload_to_wordpress(
+            folder_path,
+            useless_folder_prefix,
+            split_symbol,
+            suffix_tuple,
+            site_url,
+            username,
+            password,
+        )
+    else:
+        print("取消上传")
--- utils.py
+++ utils.py
@@ -4,28 +4,34 @@ from wordpress_xmlrpc.methods.posts import NewPost
 from markdown import markdown
 
 
-site_url = "https://yourwordpresssite.com/xmlrpc.php"
+default_site_url = "https://yourwordpresssite.com/xmlrpc.php"
 
-username = "your_username"
+default_username = "your_username"
 
-password = "your_password"
+default_password = "your_password"
 
 ## 文件夹路径
 backups_origin_md_dir = (
     "G:\code\\blog\YuQueBackups\\backups\origin_md\博客\开源经历\hertzbeat"
 )
 ## 无用的文件夹前缀
-useless_folder_prefix = "G:\\code\\blog\\YuQueBackups\\backups\\origin_md\\博客\\"
+default_useless_folder_prefix = (
+    "G:\\code\\blog\\YuQueBackups\\backups\\origin_md\\博客\\"
+)
 ## 文件夹分隔符
-split_symbol = "\\"
+default_split_symbol = "\\"
 ## 需要获取的文件后缀
-suffix_tuple = ".md"
+default_suffix_tuple = ".md"
 
 post = WordPressPost()  # 初始化post，我们要发表的文章就是post
+count = 0
 
 
 # 递归遍历文夹与子文件夹中的特定后缀文件
-def search_all_file(file_dir=backups_origin_md_dir, target_suffix_tuple=suffix_tuple):
+def search_all_file(
+    file_dir=backups_origin_md_dir,
+    target_suffix_tuple=default_suffix_tuple,
+):
     file_list = []
     # 切换到目录下
     os.chdir(file_dir)
@@ -48,12 +54,17 @@ def search_all_file(file_dir=backups_origin_md_dir, target_suffix_tuple=suffix_t
 
 def batch_upload_to_wordpress(
     file_dir=backups_origin_md_dir,
-    site_url=site_url,
-    username=username,
-    password=password,
+    useless_folder_prefix=default_useless_folder_prefix,
+    split_symbol=default_split_symbol,
+    suffix_tuple=default_suffix_tuple,
+    site_url=default_site_url,
+    username=default_username,
+    password=default_password,
+    count=count,
 ):
-    file_list = search_all_file(file_dir)
-    # print(file_list)
+    file_list = search_all_file(file_dir, suffix_tuple)
+    ## debug用
+    print(file_list)
     for file_path in file_list:
         # 将文件路径分割为文件夹和文件名
         folders = file_path.split(
@@ -77,9 +88,11 @@ def batch_upload_to_wordpress(
             password,
         )
         print(file_path, "上传成功")
+        count += 1
         # print("File Content:", file_content)
         # print("Folders:", folders)
         # print()
+    print("共上传了{}篇文章".format(count))
 
 
 def markdown_to_html(md_content):
